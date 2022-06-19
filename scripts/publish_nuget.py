9a06e0f391505adb004b6a68acd4471ef84695df
Chris Barton <chris.barton@hbkworld.com>
2022-06-19 18:12:15 +0100

    Lint changes

--- scripts/publish_nuget.py
+++ scripts/publish_nuget.py
@@ -5,6 +5,8 @@ import tempfile
 import fnmatch
 
 repo = sys.argv[2]
+
+
 def updateZip(zipname, file_glob, old, new):
     # generate a temp file
     filecontents = ""
@@ -12,14 +14,14 @@ def updateZip(zipname, file_glob, old, new):
     tmpfd, tmpname = tempfile.mkstemp(dir=os.path.dirname(zipname))
     os.close(tmpfd)
 
-    # create a temp copy of the archive without filename            
+    # create a temp copy of the archive without filename
     with zipfile.ZipFile(zipname, 'r') as zin:
         with zipfile.ZipFile(tmpname, 'w') as zout:
-            zout.comment = zin.comment # preserve the comment
+            zout.comment = zin.comment  # preserve the comment
             for item in zin.infolist():
                 if not fnmatch.fnmatch(item.filename, file_glob):
                     zout.writestr(item, zin.read(item.filename))
-                    
+
                 else:
                     filecontents = zin.read(item.filename).decode()
                     filecontents = filecontents.replace(old, new)
@@ -32,6 +34,7 @@ def updateZip(zipname, file_glob, old, new):
     # now add filename with its new data
     with zipfile.ZipFile(zipname, mode='a', compression=zipfile.ZIP_DEFLATED) as zf:
         zf.writestr(filename, filecontents)
-                
+
+
 updateZip(sys.argv[1], '*.nuspec', '  </metadata>', f'    <repository type="git" url="{repo}"/>    \n  </metadata>')
 is_windows = sys.platform.startswith('win')
