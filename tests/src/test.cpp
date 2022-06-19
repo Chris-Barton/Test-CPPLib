0a8c12a2a89c348a180e8a388a6a9fd6930a15fe
Chris Barton <chris.barton@hbkworld.com>
2022-06-19 18:02:32 +0100

    Lint changes

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,10 +1,7 @@
 #include <gtest/gtest.h>
-#include "TestMath.h"
-namespace
-{
 
-    TEST(Fib, TEST1)
-    {
-        ASSERT_EQ(TestMath::fib(1), 1);
-    }
-}
+#include "TestMath.h"
+namespace {
+
+TEST(Fib, TEST1) { ASSERT_EQ(TestMath::fib(1), 1); }
+}  // namespace
